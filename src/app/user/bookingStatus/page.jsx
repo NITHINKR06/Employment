72db2def723dcdfa462c1f54f345e8d58adfbde7
NITHINKR06 <nithinpoojari606@gmail.com>
2025-02-26 17:37:33 +0530

    updated bs and payment .

--- src/app/user/bookingStatus/page.jsx
+++ src/app/user/bookingStatus/page.jsx
@@ -7,8 +7,8 @@ import Search from '@/components/SearchBox/Search';
 export default function BookingStatu() {
 
   return (
-    <div>
-      <Search/>
+    <div className='mt-14'>
+      {/* <Search/> */}
       <div className=" flex items-center justify-center p-4">
         <BookingStatus/>
       </div>
